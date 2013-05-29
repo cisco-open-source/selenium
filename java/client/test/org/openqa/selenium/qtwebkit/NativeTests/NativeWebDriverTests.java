c0b68da1b7e28acb4fb9d6988848a363d7975d33
Nataliia Koval <nataliia.koval@globallogic.com>
2013-05-29 10:18:38 +0300

    Added FormHandlingTest

--- java/client/test/org/openqa/selenium/qtwebkit/NativeTests/NativeWebDriverTests.java
+++ java/client/test/org/openqa/selenium/qtwebkit/NativeTests/NativeWebDriverTests.java
@@ -16,12 +16,13 @@ import org.openqa.selenium.testing.JUnit4TestBase;
         FindingTest.class,
         CoordinatesTest.class,
         ClickScrollingTest.class,
-		TakesScreenshotTest.class,
-		ElementAttributeTest.class,		
+        TakesScreenshotTest.class,
+        ElementAttributeTest.class,
         ElementSelectingTest.class,
         TypingTest.class,
         BasicKeyboardInterfaceTest.class,
-		TextHandlingTest.class
+        TextHandlingTest.class,
+        FormHandlingTest.class
 })
 public class NativeWebDriverTests {
     @BeforeClass
