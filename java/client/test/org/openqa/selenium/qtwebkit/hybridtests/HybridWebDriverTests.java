ca15545cb2973574281d3b83294cff190a69bfd5
Nataliia Koval <nataliia.koval@globallogic.com>
2013-07-23 15:13:59 +0300

    Added HybridWebDriverTests

---
+++ java/client/test/org/openqa/selenium/qtwebkit/hybridtests/HybridWebDriverTests.java
@@ -0,0 +1,27 @@
+package org.openqa.selenium.qtwebkit.hybridtests;
+
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.openqa.selenium.environment.GlobalTestEnvironment;
+import org.openqa.selenium.environment.InProcessTestEnvironment;
+import org.openqa.selenium.testing.JUnit4TestBase;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+    CapabilityBrowserClassTest.class
+})
+public class HybridWebDriverTests {
+
+  @BeforeClass
+  public static void prepareCommonEnvironment() {
+    GlobalTestEnvironment.get(InProcessTestEnvironment.class);
+  }
+
+  @AfterClass
+  public static void cleanUpDriver() {
+    JUnit4TestBase.removeDriver();
+  }
+}
