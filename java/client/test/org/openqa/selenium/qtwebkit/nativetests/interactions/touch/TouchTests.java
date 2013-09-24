e152e495646d963c2c46ea7cd8162fc15d98b547
Dmytro Teteruk <dmytro.teteruk@globallogic.com>
2013-09-24 14:01:22 +0300

    Touch Tests for Native WD

---
+++ java/client/test/org/openqa/selenium/qtwebkit/nativetests/interactions/touch/TouchTests.java
@@ -0,0 +1,18 @@
+package org.openqa.selenium.qtwebkit.nativetests.interactions.touch;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.AfterClass;
+import org.openqa.selenium.testing.JUnit4TestBase;
+import org.openqa.selenium.qtwebkit.nativetests.interactions.touch.TouchInteractionTests;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        TouchInteractionTests.class,
+})
+public class TouchTests {
+    @AfterClass
+    public static void cleanUpDriver() {
+        JUnit4TestBase.removeDriver();
+    }
+}
