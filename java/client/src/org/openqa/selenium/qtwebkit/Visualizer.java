d5223ea4f4c3fd73de60a578aff08dd2f6e7511b
Vasyl Vavrychuk <vasyl.vavrychuk@globallogic.com>
2013-10-31 01:28:45 +0200

    Visualizer documented

--- java/client/src/org/openqa/selenium/qtwebkit/Visualizer.java
+++ java/client/src/org/openqa/selenium/qtwebkit/Visualizer.java
@@ -1,5 +1,14 @@
 package org.openqa.selenium.qtwebkit;
 
 public interface Visualizer {
+    /**
+     * Similar to {@link org.openqa.selenium.WebDriver#getPageSource()} but
+     * result of this one is usable for independant visualization in borwser.
+     * It means that:
+     * <ul>
+     * <li>Images are downloaded and placed using data URL scheme
+     * <li>Stylesheets are assembled with HTML
+     * </ul>
+     */
     public String getVisualizerSource();
 }
