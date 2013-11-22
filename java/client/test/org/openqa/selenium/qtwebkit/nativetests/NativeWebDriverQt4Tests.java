adf39f1ddc760b138eaf32d10b2a44c682986c0c
Nataliia Koval <nataliia.koval@globallogic.com>
2013-11-22 16:38:40 +0200

    generation xml report refactoring

--- java/client/test/org/openqa/selenium/qtwebkit/nativetests/NativeWebDriverQt4Tests.java
+++ java/client/test/org/openqa/selenium/qtwebkit/nativetests/NativeWebDriverQt4Tests.java
@@ -6,30 +6,8 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.openqa.selenium.environment.GlobalTestEnvironment;
 import org.openqa.selenium.environment.InProcessTestEnvironment;
-import org.openqa.selenium.qtwebkit.QtWebDriverExecutor;
 import org.openqa.selenium.qtwebkit.nativetests.interactions.InteractionTests;
 import org.openqa.selenium.testing.JUnit4TestBase;
-import org.openqa.selenium.testing.ReportSupplier;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.ProcessingInstruction;
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
-
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.transform.OutputKeys;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerConfigurationException;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
@@ -59,103 +37,8 @@ public class NativeWebDriverQt4Tests {
     @AfterClass
     public static void cleanUpDriver() {
         JUnit4TestBase.removeDriver();
-
-        try
-        {
-            File outFile = new File("build/test_logs/" + NativeWebDriverQt4Tests.class.getName() + "_CommandReport.xml");
-            outFile.mkdirs();
-            if (outFile.exists())
-                outFile.delete();
-
-            if (outFile.createNewFile())
-            {
-                FileOutputStream outStream = new FileOutputStream(outFile);
-
-                Transformer transformer = TransformerFactory.newInstance().newTransformer();
-                transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-                transformer.transform(new DOMSource(generateXmlReport()), new StreamResult(outStream));
-                outStream.flush();
-                outStream.close();
-            }
-        }
-        catch (FileNotFoundException ex)
-        {
-            System.out.println(ex.getMessage());
-            ex.printStackTrace();
-        }
-        catch (IOException ex)
-        {
-            System.out.println(ex.getMessage());
-            ex.printStackTrace();
-        }
-        catch (TransformerConfigurationException ex)
-        {
-            System.out.println(ex.getMessage());
-            ex.printStackTrace();
-        }
-        catch (TransformerException ex)
-        {
-            System.out.println(ex.getMessage());
-            ex.printStackTrace();
-        }
+        String testsName = NativeWebDriverQt4Tests.class.getName();
+        GlobalTestEnvironment.get(InProcessTestEnvironment.class).getStatisticCommands().writeToXml(testsName);
     }
 
-    private static Document generateXmlReport()
-    {
-        try
-        {
-            Document finalReport = DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument();
-
-            String props = "type=\"text/xsl\" href=\"" + "transform.xsl" + "\"";
-            ProcessingInstruction instruction = finalReport.createProcessingInstruction("xml-stylesheet", props);
-            finalReport.appendChild(instruction);
-
-            Element tableElement = finalReport.createElement("table");
-            finalReport.appendChild(tableElement);
-
-            HashMap<String, HashMap<String, Boolean>> commandsMap = ReportSupplier.getCommandMap();
-            Iterator it = commandsMap.keySet().iterator();
-
-            // process all commands
-            while (it.hasNext())
-            {
-                String key = (String)it.next();
-                String path = QtWebDriverExecutor.getCommandPath(key);
-                String method = QtWebDriverExecutor.getCommandMethod(key);
-
-                if (null != path && null != method)
-                {
-                    Element command = finalReport.createElement("command");
-                    command.setAttribute("name", key);
-                    command.setAttribute("path", path);
-                    command.setAttribute("method", method);
-
-                    // add command tests
-                    HashMap<String, Boolean> tests = commandsMap.get(key);
-
-                    Iterator iter = tests.entrySet().iterator();
-                    while (iter.hasNext())
-                    {
-                        Map.Entry pair = (Map.Entry)iter.next();
-                        Element test = finalReport.createElement("test");
-                        test.setAttribute("name", (String)pair.getKey());
-                        String passed = ((Boolean)pair.getValue()).toString();
-                        test.setAttribute("passed", passed);
-
-                        command.appendChild(test);
-                    }
-
-                    tableElement.appendChild(command);
-                }
-
-            } // while (it.hasNext())
-
-            return finalReport;
-        }
-        catch (Exception ex)
-        {
-            ex.printStackTrace();
-            return null;
-        }
-    }
 }
