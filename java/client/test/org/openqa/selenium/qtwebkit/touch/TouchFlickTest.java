00df1827bbd29268564ca6b43d099d51cfde412a
Dmytro Teteruk <dmytro.teteruk@globallogic.com>
2013-11-25 19:01:12 +0200

    Remove old touch test

--- java/client/test/org/openqa/selenium/qtwebkit/touch/TouchFlickTest.java
+++ java/client/test/org/openqa/selenium/qtwebkit/touch/TouchFlickTest.java
@@ -23,47 +23,6 @@ public class TouchFlickTest extends TouchTestBase {
 
     @NeedsFreshDriver
     @Test
-    public void testCanFlickHorizontallyFromWebElement() {
-        driver.get(pages.longContentPage);
-
-        WebElement toFlick = driver.findElement(By.id("imagestart"));
-        WebElement link = driver.findElement(By.id("link1"));
-        int x = link.getLocation().x;
-        // The element is located at the right of the page,
-        // so it is not initially visible on the screen.
-        assertTrue("Expected x > 1500, but got x = " + x, x > 1500);
-
-        Action flick = getBuilder(driver).flick(toFlick, -1000, 0, FlickAction.SPEED_NORMAL)
-                .build();
-        flick.perform();
-
-        x = ((Long)((JavascriptExecutor) driver).executeScript("return document.body.scrollLeft")).intValue();
-        //If speed normal we scroll xoffset pixel
-        assertTrue("Expected x == 1000, but got x = " + x, x == 1000);
-    }
-
-    @NeedsFreshDriver
-    @Test
-    public void testCanFlickHorizontallyFastFromWebElement() {
-        driver.get(pages.longContentPage);
-
-        WebElement toFlick = driver.findElement(By.id("imagestart"));
-        WebElement link = driver.findElement(By.id("link2"));
-        int x = link.getLocation().x;
-        // The element is located at the right of the page,
-        // so it is not initially visible on the screen.
-        assertTrue(x > 3500);
-
-        Action flick = getBuilder(driver).flick(toFlick, -400, 0, FlickAction.SPEED_FAST)
-                .build();
-        flick.perform();
-        x = ((Long)((JavascriptExecutor) driver).executeScript("return document.body.scrollLeft")).intValue();
-        //If speed fast we scroll 2*xoffset pixel
-        assertTrue("Expected x == 800, but got: " + x, x == 800);
-    }
-
-    @NeedsFreshDriver
-    @Test
     public void testCanFlickHorizontally() {
         driver.get(pages.clicksPage);
         driver.get(pages.longContentPage);
@@ -99,46 +58,6 @@ public class TouchFlickTest extends TouchTestBase {
         x = ((Long)((JavascriptExecutor) driver).executeScript("return document.body.scrollLeft")).intValue();
         //Scroll 3*xspeed pixel
         assertTrue("Got: " + x, x == 2250);
-    }
-
-    @NeedsFreshDriver
-    @Test
-    public void testCanFlickVerticallyFromWebElement() {
-        driver.get(pages.longContentPage);
-
-        WebElement link = driver.findElement(By.id("link3"));
-        int y = link.getLocation().y;
-        // The element is located at the bottom of the page,
-        // so it is not initially visible on the screen.
-        assertTrue(y > 4200);
-
-        WebElement toFlick = driver.findElement(By.id("imagestart"));
-        Action flick = getBuilder(driver).flick(toFlick, 0, -600, FlickAction.SPEED_NORMAL)
-                .build();
-        flick.perform();
-        y = ((Long)((JavascriptExecutor) driver).executeScript("return document.body.scrollTop")).intValue();
-        //If speed normal we scroll yoffset pixel
-        assertTrue("Expected y == 600, but got: " + y, y == 600);
-    }
-
-    @NeedsFreshDriver
-    @Test
-    public void testCanFlickVerticallyFastFromWebElement() {
-        driver.get(pages.longContentPage);
-
-        WebElement link = driver.findElement(By.id("link4"));
-        int y = link.getLocation().y;
-        // The element is located at the bottom of the page,
-        // so it is not initially visible on the screen.
-        assertTrue(y > 8700);
-
-        WebElement toFlick = driver.findElement(By.id("imagestart"));
-        Action flick = getBuilder(driver).flick(toFlick, 0, -600, FlickAction.SPEED_FAST)
-                .build();
-        flick.perform();
-        y = ((Long)((JavascriptExecutor) driver).executeScript("return document.body.scrollTop")).intValue();
-        //If speed normal we scroll 2*yoffset pixel
-        assertTrue("Expected y == 1200, but got: " + y, y == 1200);
     }
 
     @NeedsFreshDriver
--- java/client/test/org/openqa/selenium/qtwebkit/touch/TouchScrollTest.java
+++ java/client/test/org/openqa/selenium/qtwebkit/touch/TouchScrollTest.java
@@ -24,46 +24,6 @@ public class TouchScrollTest extends TouchTestBase {
 
     @NeedsFreshDriver
     @Test
-    public void testCanScrollVerticallyFromWebElement() {
-        driver.get(pages.longContentPage);
-
-        WebElement link = driver.findElement(By.id("link3"));
-        int y = link.getLocation().y;
-        // The element is located at the right of the page,
-        // so it is not initially visible on the screen.
-        assertTrue("Expected y > 4200, but got y = " + y, y > 4200);
-
-        WebElement toScroll = driver.findElement(By.id("imagestart"));
-        Action scroll = getBuilder(driver).scroll(toScroll, 0, -800).build();
-        scroll.perform();
-
-        y = ((Long)((JavascriptExecutor) driver).executeScript("return document.body.scrollTop")).intValue();
-        // Scroll position should change
-        assertTrue("Expected y == 1000, but got y = " + y, y == 800);
-    }
-
-    @NeedsFreshDriver
-    @Test
-    public void testCanScrollHorizontallyFromWebElement() {
-        driver.get(pages.longContentPage);
-
-        WebElement link = driver.findElement(By.id("link1"));
-        int x = link.getLocation().x;
-        // The element is located at the right of the page,
-        // so it is not initially visible on the screen.
-        assertTrue("Expected x > 1500, but got x = " + x, x > 1500);
-
-        WebElement toScroll = driver.findElement(By.id("imagestart"));
-        Action scroll = getBuilder(driver).scroll(toScroll, -1000, 0).build();
-        scroll.perform();
-
-        x = ((Long)((JavascriptExecutor) driver).executeScript("return document.body.scrollLeft")).intValue();
-        // Scroll position should change
-        assertTrue("Expected x == 1000, but got x = " + x, x == 1000);
-    }
-
-    @NeedsFreshDriver
-    @Test
     public void testCanScrollVertically() {
         driver.get(pages.longContentPage);
 
