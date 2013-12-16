2c6de2091bfc3164271f6b19179d7a0f71dc81cc
Vasyl Vavrychuk <vasyl.vavrychuk@globallogic.com>
2013-12-16 13:05:31 +0200

    extracted some code from test function

--- java/client/test/org/openqa/selenium/qtwebkit/visualizer_tests/QtWebDriverVisualizerTest.java
+++ java/client/test/org/openqa/selenium/qtwebkit/visualizer_tests/QtWebDriverVisualizerTest.java
@@ -1,26 +1,38 @@
 package org.openqa.selenium.qtwebkit.visualizer_tests;
 
-import org.junit.*;
-
-import org.openqa.selenium.*;
-import org.openqa.selenium.qtwebkit.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.qtwebkit.QtWebDriverExecutor;
+import org.openqa.selenium.qtwebkit.QtWebDriverService;
+import org.openqa.selenium.qtwebkit.QtWebDriverServiceExecutor;
+import org.openqa.selenium.qtwebkit.QtWebKitDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.service.DriverService;
 import org.openqa.selenium.testing.JUnit4TestBase;
-import org.openqa.selenium.testing.NeedsLocalEnvironment;
 
 import java.util.Set;
 import java.util.concurrent.Callable;
 
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
 import static org.openqa.selenium.TestWaiter.waitFor;
-import static org.openqa.selenium.WaitingConditions.*;
+import static org.openqa.selenium.WaitingConditions.elementToExist;
+import static org.openqa.selenium.WaitingConditions.elementValueToEqual;
+import static org.openqa.selenium.WaitingConditions.newWindowIsOpened;
+import static org.openqa.selenium.WaitingConditions.pageTitleToBe;
+import static org.openqa.selenium.WaitingConditions.windowHandleCountToBe;
+import static org.openqa.selenium.WaitingConditions.windowToBeSwitchedToWithName;
 
 public class QtWebDriverVisualizerTest extends JUnit4TestBase {
   protected WebDriver driver2;
-  private String wd2IP;
+  private String webDriverJsWindowHandle;
+  private String visualizerWindowHandle;
 
   @Before
   public void createDriver() throws Exception {
@@ -32,49 +44,35 @@ public class QtWebDriverVisualizerTest extends JUnit4TestBase {
     DriverService webDriver2Service = QtWebDriverService.createDefaultService();
     QtWebDriverExecutor webDriver2Executor = new QtWebDriverServiceExecutor(webDriver2Service);
     driver2 = new QtWebKitDriver(webDriver2Executor, capabilities);
-    wd2IP = webDriver2Service.getUrl().toExternalForm();
+
+    String realWebDriverUrl = webDriver2Service.getUrl().toExternalForm();
+    driver.get(realWebDriverUrl + "/WebDriverJsDemo.html");
+    webDriverJsWindowHandle = driver.getWindowHandle();
+
+    WebElement webDriverUrlPort = driver.findElement(By.name("webDriverUrlPort"));
+    webDriverUrlPort.clear();
+    webDriverUrlPort.sendKeys(realWebDriverUrl);
   }
 
   @After
-  public void theerDown() throws Exception {
+  public void tearDown() throws Exception {
     driver.quit();
     driver2.quit();
   }
 
   @Test
   public void canOpenLinkAndTypeText() {
-    driver.get(wd2IP + "/WebDriverJsDemo.html");
-
-    WebElement webDriverUrlPort = driver.findElement(By.name("webDriverUrlPort"));
-    webDriverUrlPort.clear();
-    webDriverUrlPort.sendKeys(wd2IP);
-
-    WebElement webPage = driver.findElement(By.name("webPage"));
-    webPage.clear();
-    webPage.sendKeys(pages.clicksPage);
-
-    String currentHandle = driver.getWindowHandle();
     Set<String> currentWindowHandles = driver.getWindowHandles();
-
-    driver.findElement(By.xpath("//input[@value='Source']")).click();
+    setWebPage(pages.clicksPage);
+    source();
     waitFor(newWindowIsOpened(driver, currentWindowHandles));
 
     driver2.findElement(By.id("normal")).click();
     waitFor(pageTitleToBe(driver2, "XHTML Test Page"));
 
-    driver.findElement(By.xpath("//input[@value='Source']")).click();
-    waitFor(windowHandleCountToBe(driver, 2));
-
-    Set<String> allWindowHandles = driver.getWindowHandles();
-    assertEquals(2, allWindowHandles.size());
-    String handle = (String) allWindowHandles.toArray()[1];
-
-    if (currentHandle.equalsIgnoreCase(handle)) {
-      handle = (String) allWindowHandles.toArray()[0];
-    }
-
-    driver.switchTo().window(handle);
-    waitFor(windowToBeSwitchedToWithName(driver, handle));
+    source();
+    driver.switchTo().window(visualizerWindowHandle);
+    waitFor(windowToBeSwitchedToWithName(driver, visualizerWindowHandle));
 
     String typingText = "TheTypingText";
     String expectedText = "change" + typingText;
@@ -91,6 +89,28 @@ public class QtWebDriverVisualizerTest extends JUnit4TestBase {
 
     waitFor(elementValueToEqual(inputField2, expectedText));
     assertThat(inputField2.getAttribute("value"), equalTo(expectedText));
+  }
+
+  private void setWebPage(String webPage) {
+    WebElement webPageUrl = driver.findElement(By.name("webPage"));
+    webPageUrl.clear();
+    webPageUrl.sendKeys(webPage);
+  }
+
+  private void source() {
+    driver.findElement(By.xpath("//input[@value='Source']")).click();
+    waitFor(windowHandleCountToBe(driver, 2));
+
+    Set<String> allWindowHandles = driver.getWindowHandles();
+    assertEquals(2, allWindowHandles.size());
+
+    visualizerWindowHandle = null;
+    for (String handle : allWindowHandles) {
+      if (!handle.equalsIgnoreCase(webDriverJsWindowHandle)) {
+        visualizerWindowHandle = handle;
+      }
+    }
+    assertNotNull(visualizerWindowHandle);
   }
 
   public static Callable<WebElement> activeElementToBe(
