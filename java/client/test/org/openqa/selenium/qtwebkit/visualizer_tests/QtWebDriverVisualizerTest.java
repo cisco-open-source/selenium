d66b12f4faa9fb78aad6162224b55a7a8372a046
Vasyl Vavrychuk <vasyl.vavrychuk@globallogic.com>
2014-01-09 09:26:22 +0200

    added touch tests

--- java/client/test/org/openqa/selenium/qtwebkit/visualizer_tests/QtWebDriverVisualizerTest.java
+++ java/client/test/org/openqa/selenium/qtwebkit/visualizer_tests/QtWebDriverVisualizerTest.java
@@ -3,9 +3,12 @@ package org.openqa.selenium.qtwebkit.visualizer_tests;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.multitouch.MultiTouchActions;
 
 import java.util.Set;
 
+import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
@@ -14,6 +17,20 @@ import static org.openqa.selenium.WaitingConditions.*;
 import static org.openqa.selenium.qtwebkit.visualizer_tests.WaitingConditions.activeElementToBe;
 
 public class QtWebDriverVisualizerTest extends QtWebDriverJsBaseTest {
+
+  private static final long TOUCH_TIME_OUT = 20;
+
+  private void rotateElement(String elementId, int angle) {
+    WebElement toRotate = driver.findElement(By.id(elementId));
+    Action rotate = new MultiTouchActions(driver).pinchRotate(toRotate, angle).build();
+    rotate.perform();
+  }
+
+  private void zoomElement(String elementId, double scale) {
+    WebElement toZoom = driver.findElement(By.id(elementId));
+    Action zoom = new MultiTouchActions(driver).pinchZoom(toZoom, scale).build();
+    zoom.perform();
+  }
 
   @Test
   public void canOpenLinkAndTypeText() {
@@ -55,5 +72,45 @@ public class QtWebDriverVisualizerTest extends QtWebDriverJsBaseTest {
     waitFor(pageTitleToBe(targetDriver, "XHTML Test Page"));
 
     assertEquals("We do not proceed by links in visualizer", visualizerTitle, driver.getTitle());
+  }
+
+  @Test
+  public void canPinchRotate() {
+    page.setWebPage(pages.pinchTouchTest);
+    driver.switchTo().window(page.clickSource());
+
+    rotateElement("picture", 35);
+    WebElement result = targetDriver.findElement(By.id("result_rotate"));
+    waitFor(elementTextToEqual(result, "35"), TOUCH_TIME_OUT, SECONDS);
+  }
+
+  @Test
+  public void canPinchRotateNegativeDegree() {
+    page.setWebPage(pages.pinchTouchTest);
+    driver.switchTo().window(page.clickSource());
+
+    rotateElement("picture", -35);
+    WebElement result = targetDriver.findElement(By.id("result_rotate"));
+    waitFor(org.openqa.selenium.WaitingConditions.elementTextToEqual(result, "-35"), TOUCH_TIME_OUT, SECONDS);
+  }
+
+  @Test
+  public void canPinchZoomOut() {
+    page.setWebPage(pages.pinchTouchTest);
+    driver.switchTo().window(page.clickSource());
+
+    zoomElement("picture", 2.5);
+    WebElement result = targetDriver.findElement(By.id("result_scale"));
+    waitFor(org.openqa.selenium.WaitingConditions.elementTextToEqual(result, "2.5"), TOUCH_TIME_OUT, SECONDS);
+  }
+
+  @Test
+  public void canPinchZoomIn() {
+    page.setWebPage(pages.pinchTouchTest);
+    driver.switchTo().window(page.clickSource());
+
+    zoomElement("picture", 0.5);
+    WebElement result = targetDriver.findElement(By.id("result_scale"));
+    waitFor(org.openqa.selenium.WaitingConditions.elementTextToEqual(result, "0.5"), TOUCH_TIME_OUT, SECONDS);
   }
 }
